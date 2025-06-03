e4651c2bcbf7113bcf520d611018509bfe7ca2d4
trishonbaidaya7399 <githubtrishon@gmail.com>
2025-06-03 23:11:04 +0600

    updated

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ connectToDatabase()
 app.use(
   cors({
     // origin: "http://localhost:3000",
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
     credentials: true,
   })
 );
